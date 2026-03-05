23f6d869c3dcdff0f8c83ccf09f323d15b781940
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2026-03-05 11:51:50 +0100

    added div for button in card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,10 +8,12 @@ export default function Card({ game, compareList, toggleCompare }) {
         <div className="card p-2 my-2" key={game.id}>
             <h4>Titolo del gioco: {game.title}</h4>
             <p>categoria: {game.category}</p>
-            <Link className="btn btn-secondary my-2" to={`/games/${game.id}`}>Dettagli</Link>
-            <button className="btn btn-secondary" onClick={() => toggleCompare(game)}>
-                {compareList.find(c => c.id === game.id) ? "Rimuovi da confronto" : "Confronta"}
-            </button>
+            <div className="buttonBox">
+                <Link className="btn btn-secondary" to={`/games/${game.id}`}>Dettagli</Link>
+                <button className="btn btn-secondary mx-1" onClick={() => toggleCompare(game)}>
+                    {compareList.find(c => c.id === game.id) ? "Rimuovi da confronto" : "Confronta"}
+                </button>
+            </div>
         </div>
     )
 
