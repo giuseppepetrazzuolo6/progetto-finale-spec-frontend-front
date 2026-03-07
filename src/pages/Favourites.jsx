ac38b1db7a059fb7c7cd317511cf4494c667943d
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2026-03-07 11:50:23 +0100

    Added button to remove all favorites

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -3,12 +3,12 @@ import { GlobalContext } from "../context/GlobalContext"
 import { Link } from "react-router-dom"
 
 export default function Favourites() {
-    const { favList, removeFromFav } = useContext(GlobalContext)
-
+    const { favList, removeFromFav, clearFav } = useContext(GlobalContext)
 
     return (
-        <div className="container py-4">
+        <div className="container py-4 vh-100">
             <h1 className="mb-4">Pagina dei preferiti</h1>
+
             {favList.length === 0 ? (
                 <div className="text-center mt-5">
                     <h3>Lista dei preferiti vuota</h3>
@@ -16,20 +16,29 @@ export default function Favourites() {
                         Torna alla lista giochi
                     </Link>
                 </div>
-            ) :
-                (favList.map(game => (
-                    <div className="card p-3 my-2" key={game.id}>
-                        <div className="d-flex justify-content-between align-items-center">
-                            <h5 className="mb-0">{game.title}</h5>
-                            <button
-                                className="btn btn-outline-danger btn-sm"
-                                onClick={() => removeFromFav(game.id)}>
-                                <i className="bi bi-trash"></i>
-                            </button>
+            ) : (
+                <section>
+                    {favList.map(game => (
+                        <div className="card p-3 my-2" key={game.id}>
+                            <div className="d-flex justify-content-between align-items-center">
+                                <h5 className="mb-0">{game.title}</h5>
+                                <button
+                                    className="btn btn-outline-danger btn-sm"
+                                    onClick={() => removeFromFav(game.id)}>
+                                    <i className="bi bi-trash"></i>
+                                </button>
+                            </div>
                         </div>
+                    ))}
+                    <div className="text-end">
+                        <button
+                            className="btn btn-outline-danger"
+                            onClick={clearFav}>
+                            Elimina tutti
+                        </button>
                     </div>
-                ))
-                )}
+                </section>
+            )}
         </div>
     )
 }
